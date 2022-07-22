143afbcb535458459981e3d5a29c3459e4eaf430
par4m <param2k3@gmail.com>
2022-07-22 23:43:32 +0530

    process.sh

--- display.sh
+++ display.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xrandr --output eDP-1 --set "Broadcast RGB" "Full"
-xgamma -rgamma 0.85 -bgamma 0.85 -ggamma 0.85
+xrandr --output eDP1 --set "Broadcast RGB" "Full"
+xgamma -quiet -rgamma 0.88 -ggamma 0.85 -bgamma 0.70
