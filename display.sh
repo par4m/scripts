c90aac9338a1525aabf1552dc62fea5b40cd36c6
par4m <param2k3@gmail.com>
2023-06-08 01:27:50 +0530

    monitor fix

--- display.sh
+++ display.sh
@@ -2,5 +2,6 @@
 
 # xrandr --output eDP1 --set "Broadcast RGB" "Full"
 # xrandr --auto --output HDMI1 --primary --mode 1920x1200 --output LVDS1 --below eDP1 --mode 1920x1080
-xrandr --output eDP1 --set "Broadcast RGB" "Full" --output HDMI1 --mode 1920x1080 --left-of eDP1
+xrandr --auto --output eDP1 --primary --set "Broadcast RGB" "Full" --output DP1 --right-of eDP1 --mode 1920x1080
+
 xgamma -quiet -rgamma 0.88 -ggamma 0.85 -bgamma 0.70
